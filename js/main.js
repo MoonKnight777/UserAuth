de07b1fd205d15053fa493c499bbbb210dfb15cf
MoonKnight777 <moonkinght4505@gmail.com>
2023-10-29 16:55:03 +0530

    BasicTest

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const response = fetch('./js/info.json')
+    .then((info) => {
+        const data = info.json();
+    })
+
+function returnvalue() {
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (username === data.username && password === data.password) {
+        console.log("logged in")
+        let head = document.querySelector(".sts");
+        head.innerHTML = "Unlocked";
+    }
+}
+
+returnvalue();
