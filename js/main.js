c0121729c4aca24bc82f6ceae721b01e8401b8c8
MoonKnight777 <moonkinght4505@gmail.com>
2023-10-30 08:07:55 +0530

    Function Fixed

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-const response = fetch('./js/info.json')
-    .then((info) => {
-        const data = info.json();
-    })
 
 function returnvalue() {
+    
+    fetch('./js/info.json')
+        .then((info) => {
+            return info.json();
+        })
+        .then((data) => {
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    if (username === data.username && password === data.password) {
-        console.log("logged in")
-        let head = document.querySelector(".sts");
-        head.innerHTML = "Unlocked";
-    }
+            let username = document.getElementById("username").value;
+            let password = document.getElementById("password").value;
+            if (username === data.username && password === data.password) {
+                console.log("logged in")
+                let head = document.querySelector(".sts");
+                head.innerHTML = "Unlocked";
+            }
+
+        })
+    
 }
 
-returnvalue();
